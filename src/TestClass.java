7c19203d30a08abb56e554a6a2515ca38f03f24e
Stanislav <kaplanst@mail.ru>
2022-05-25 16:03:22 -0400

    11

--- src/TestClass.java
+++ src/TestClass.java
@@ -7,13 +7,9 @@ public class TestClass {
         Scanner inputData = new Scanner(System.in);  // Create a Scanner object
         System.out.println("Encrypt or Decrypt (E/D)");
         String objective = inputData.nextLine();  // Input text
-        int number = inputData.nextInt();
         inputData = new Scanner(System.in);
         String simple = inputData.nextLine();
-
         System.out.println("Objective " + objective);
-
-        System.out.println("Number " + number);
         System.out.println("Simple " + simple);
         System.out.print("\033[H\033[2J");
         System.out.flush();
